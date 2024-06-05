a058dd6a4d105a7f325f20dfc4aca0c055bc4288
SantiagoZarate <santiago2001zarate@gmail.com>
2024-06-05 01:14:39 +0000

    fix: functions are handlers funcs are available when running main.go

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -14,16 +15,16 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome to my first golang API!"))
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func SnippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Returning snippet with id %d...", id))
+	fmt.Println("Returning snippet with id %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func SnippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allowed", http.MethodPost)
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	server := http.NewServeMux()
-	server.HandleFunc("/", home)
-	server.HandleFunc("/snippet/view", snippetView)
-	server.HandleFunc("/snippet/create", snippetCreate)
+	server.HandleFunc("/", Home)
+	server.HandleFunc("/snippet/view", SnippetView)
+	server.HandleFunc("/snippet/create", SnippetCreate)
 
 	log.Println("Starting server on port 4000...")
 	err := http.ListenAndServe(":4000", server)
