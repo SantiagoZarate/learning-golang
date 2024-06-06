77cd3198d0b71a08bdbf684409eff5f18bf6d68d
SantiagoZarate <santiago2001zarate@gmail.com>
2024-06-05 21:11:28 -0300

    feat: add dockerfile and modify docker compose file for client service

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 8000, // This is the port which we will use in docker
+  },
+  preview: {
+    port: 8000,
+    host: true
+  },
 })
