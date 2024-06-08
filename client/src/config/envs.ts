abc9ea62e5c4dff35ec6cf9e6be5777ea7f7b113
SantiagoZarate <santiago2001zarate@gmail.com>
2024-06-08 14:45:45 -0300

    fix: pass envs to prod client through args in dockerfile

--- client/src/config/envs.ts
+++ client/src/config/envs.ts
@@ -3,11 +3,13 @@ import { z } from 'zod'
 console.log(import.meta.env)
 
 const API_URL = import.meta.env.VITE_API_URL ?? ""
+const APP_VERSION = import.meta.env.VITE_APP_VERSION
 const DEVELOPMENT = import.meta.env.DEV
 
 const envsSchema = z.object({
   API_URL: z.string().trim(),
-  DEVELOPMENT: z.boolean()
+  DEVELOPMENT: z.boolean(),
+  APP_VERSION: z.string()
 });
 
-export const envs = envsSchema.parse({ API_URL, DEVELOPMENT })
+export const envs = envsSchema.parse({ API_URL, DEVELOPMENT, APP_VERSION })
