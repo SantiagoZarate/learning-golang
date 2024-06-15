8f8e829ca514e5c0bc88207655c4acd4ab9991ca
SantiagoZarate <santiago2001zarate@gmail.com>
2024-06-15 18:54:00 -0300

    fix: set cors middleware

--- auth-service/src/middlewares/setMiddlewares.ts
+++ auth-service/src/middlewares/setMiddlewares.ts
@@ -3,6 +3,7 @@ import express, { Application, json, urlencoded } from "express";
 import morgan from "morgan";
 import cookieParser from 'cookie-parser'
 import path from "path";
+import cors from 'cors'
 
 export function setMiddlewares(server: Application) {
   server.use(express.static(path.resolve("public")))
@@ -10,6 +11,7 @@ export function setMiddlewares(server: Application) {
   server.use(urlencoded({
     extended: true
   }))
+  server.use(cors())
   server.use(cookieParser())
   envs.MODE === "dev" && server.use(morgan("dev"))
 }
