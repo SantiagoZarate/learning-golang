f4ee0008e5cb9798926b6312910f5730c10e580d
SantiagoZarate <santiago2001zarate@gmail.com>
2024-06-16 20:06:24 -0300

    style: add popup settings menu

--- client/src/common/Header.tsx
+++ client/src/common/Header.tsx
@@ -1,41 +1,60 @@
 import { MoonIcon } from "@/components/icons/MoonIcon";
 import { SunIcon } from "@/components/icons/SunIcon";
+import { UpsideArrow } from "@/components/icons/UpsideArrow";
 import { Button } from "@/components/ui/button";
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
 import { useGlobalContext } from "@/hooks/useGlobalContext";
 import { Link } from "react-router-dom";
 
 export function Header() {
-  const { userIsLogged, toggleTheme, isDarkTheme } = useGlobalContext()
+  const { toggleTheme, isDarkTheme } = useGlobalContext()
 
   return (
     <header className="z-50 absolute w-full">
       <div className="p-4 max-w-screen-lg flex justify-between items-center mx-auto">
-        <p className="bg-stone-800 aspect-square p-4 rounded-xl">
+        <p className="bg-stone-800 aspect-square p-4 rounded-xl text-accent font-bold">
           SB
         </p>
-        <section className="flex items-center gap-2">
-          <Button className="w-fit space-x-0" onClick={toggleTheme}>
-            {
-              isDarkTheme
-                ? <MoonIcon />
-                : <SunIcon />
-            }
-          </Button>
-          {
-            userIsLogged
-              ?
-              <Link to={"/account"}>
-                <Button variant={'secondary'}>
-                  mi cuenta
-                </Button>
-              </Link>
-              :
-              <Link to={"/login"} className="underline">
-                <Button>
-                  login
-                </Button>
-              </Link>
-          }
+        <section className="flex items-center divide-x">
+          <Popover>
+            <PopoverTrigger className="hover:translate-y-1 transition p-2 mx-2">
+              <UpsideArrow />
+            </PopoverTrigger>
+            <PopoverContent className="flex flex-col w-fit p-0 bg-background">
+              <ul className="flex flex-col divide-y text-sm">
+                <li>
+                  <p className="font-bold text-center py-1 text-xs bg-muted">Theme</p>
+                  <button className="w-fit space-x-0 flex gap-2 hover:bg-muted transition px-4 py-2" onClick={toggleTheme}>
+                    {
+                      isDarkTheme
+                        ? <MoonIcon />
+                        : <SunIcon />
+                    }
+                    toggle
+                  </button>
+                </li>
+                <li>
+                  <p className="font-bold py-1 text-center capitalize text-xs bg-muted">language</p>
+                  <ul className="flex flex-col divide-y *:px-4 *:py-2 *:text-center capitalize">
+                    <li>spanish</li>
+                    <li>english</li>
+                  </ul>
+                </li>
+              </ul>
+            </PopoverContent>
+          </Popover>
+          <div className="flex gap-2 px-2">
+            <Link to={"/register"}>
+              <Button variant={'ghost'} className="capitalize">
+                log in
+              </Button>
+            </Link>
+            <Link to={"/login"}>
+              <Button className="capitalize">
+                sign up
+              </Button>
+            </Link>
+          </div>
         </section>
       </div>
     </header>
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 export function LandingPage() {
   return (
@@ -7,14 +8,22 @@ export function LandingPage() {
       <div className="min-h-screen max-w-screen-lg mx-auto h-full grid grid-cols-2">
         <article className="z-10 flex flex-col justify-center gap-2">
           <h1 className="text-[72px] font-bold text-accent">Snippetbox</h1>
-          <p>its like twitter but simpler</p>
-          <p>Share what are you thinking at any time!</p>
-          <Button className="uppercase w-fit rounded-full font-bold bg-card text-secondary">
-            Try it now!
-          </Button>
+          <p>Share what are you thinking at any time and check other people's snippets out!</p>
+          <footer className="flex gap-4">
+            <Link to={"/home"}>
+              <Button variant={'secondary'} className="uppercase w-fit rounded-full font-bold bg-card text-secondary">
+                Try it now!
+              </Button>
+            </Link>
+            <Link to={"/login"}>
+              <Button className="uppercase w-fit rounded-full font-bold bg-card text-secondary">
+                Create an account
+              </Button>
+            </Link>
+          </footer>
         </article>
         <article className="relative flex items-center p-8">
-          <div className="w-full border-8 border-stone-700 rounded-3xl aspect-square overflow-hidden">
+          <div className="shadow-xl w-full border-8 border-border rounded-3xl aspect-square overflow-hidden">
             <div className="w-full h-full bg-stone-900 p-4">
 
             </div>
