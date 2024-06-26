d7274abd729aad3748f3aab8f8cd5f6a37d9f4cd
SantiagoZarate <santiago2001zarate@gmail.com>
2024-06-26 20:39:33 -0300

    fix: modify ddl when starting db

--- init.sql
+++ init.sql
@@ -3,9 +3,27 @@ CREATE TABLE snippet (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     content TEXT NOT NULL,
+    private BOOLEAN DEFAULT false,
     created TIMESTAMP NOT NULL,
     expires TIMESTAMP NOT NULL
 );
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(100) NOT NULL,
+  password TEXT NOT NULL,
+  role VARCHAR(12),
+  created_at DATE NOT NULL DEFAULT current_date
+);
+
+CREATE TABLE snippet_shared_with(
+  user_id INT,
+  snippet_id INT
+);
+
+ALTER TABLE snippet_shared_with ADD CONSTRAINT snippet_shared_with_user_fk FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE snippet_shared_with ADD CONSTRAINT snippet_shared_with_snippet_fk FOREIGN KEY (snippet_id) REFERENCES snippet (id);
+ALTER TABLE snippet_shared_with ADD CONSTRAINT snippet_shared_with_pk PRIMARY KEY (user_id, snippet_id);
 
 CREATE OR REPLACE FUNCTION createSnippet(_title VARCHAR(100), _content TEXT, _expires INT) RETURNS INT AS $$
 DECLARE
@@ -20,6 +38,24 @@ BEGIN
   RETURNING id INTO snippet_id;
 
   RETURN snippet_id;
+END; $$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION addUsersSharedWithSnippet(_user_id INT, _snippet_id INT) RETURNS BOOLEAN AS $$
+DECLARE
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM users WHERE id = _user_id) THEN 
+    RAISE EXCEPTION 'There is no user with id = [%]', _user_id;
+  END IF;
+
+  IF NOT EXISTS (SELECT 1 FROM snippet WHERE id = _snippet_id) THEN 
+    RAISE EXCEPTION 'There is no snippet with id = [%]', _snippet_id;
+  END IF;
+
+  INSERT INTO snippet_shared_with (user_id, snippet_id)
+  VALUES (_user_id, _snippet_id)
+  ON CONFLICT (user_id, snippet_id) DO NOTHING;
+
+  RETURN true;
 END; $$ language plpgsql;
 
 -- Expired snippet
