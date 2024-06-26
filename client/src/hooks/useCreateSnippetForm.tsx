ffe89fc77279be0734e73b60dde3aa17bef60448
SantiagoZarate <santiago2001zarate@gmail.com>
2024-06-26 15:22:12 -0300

    refactor: move create snippet form to its own hook

---
+++ client/src/hooks/useCreateSnippetForm.tsx
@@ -0,0 +1,135 @@
+import { SnippetFormType, createSnippetSchema } from "@/helpers/createSnippetSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useGlobalContext } from "./useGlobalContext";
+import { User } from "@/types/user";
+import snippetAPI from '@/services/snippets'
+import { type Snippet as SnippetType } from "@/types/snippet";
+import data from '@/data/users.json'
+
+export function useCreateSnippetForm() {
+  const { userIsLogged, getToken } = useGlobalContext()
+  const form = useForm<SnippetFormType>({
+    resolver: zodResolver(createSnippetSchema),
+    defaultValues: {
+      content: "",
+      title: "",
+      expires: 1,
+      sharedWith: []
+    }
+  })
+  const queryClient = useQueryClient()
+  const [users, setUsers] = useState<{ available: User[], selected: User[] }>({ available: data, selected: [] })
+
+  const { isPending, mutate } = useMutation({
+    mutationKey: ["snippets"],
+    mutationFn: (data: SnippetFormType) => snippetAPI.createSnippet(data, getToken()),
+    onMutate: async (newSnippet: SnippetFormType) => {
+      await queryClient.cancelQueries({ queryKey: ["snippets"] })
+      const previousSnippets = queryClient.getQueryData(["snippets"])
+
+      queryClient.setQueryData(["snippets"], (oldData: SnippetType[]) => {
+        if (oldData === null) {
+          return [newSnippet]
+        }
+        return [
+          {
+            ...newSnippet,
+            id: oldData.length + 1,
+            isRecentlyAdded: true
+          },
+          ...oldData,
+        ]
+      })
+      return { previousSnippets }
+    },
+    onError: (error, _, context) => {
+      console.log(error)
+      if (context?.previousSnippets !== undefined) {
+        queryClient.setQueryData(["snippets"], context.previousSnippets)
+      }
+    },
+    onSettled: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["snippets"]
+      })
+    },
+    onSuccess: () => {
+      console.log("Snippet added succesfully")
+      form.reset()
+      setUsers((prevState) => {
+        return {
+          available: prevState.available.concat(prevState.selected),
+          selected: []
+        }
+      })
+    },
+  });
+
+  const incrementExpireDay = () => {
+    const currentValue = form.getValues("expires")
+    form.setValue("expires", Number(currentValue) + 1)
+  }
+
+  const decrementExpireDay = () => {
+    const currentValue = form.getValues("expires")
+    if (currentValue === undefined) {
+      form.setValue("expires", 1)
+      return
+    }
+    if (currentValue <= 1) {
+      return
+    }
+    form.setValue("expires", Number(currentValue) - 1)
+  }
+
+  const addSharedUser = (id: number) => {
+    const user = users.available.find(u => u.id === id)
+    if (user === undefined) {
+      return
+    }
+
+    setUsers((prevState) => {
+      return {
+        available: prevState.available.filter(user => user.id !== id),
+        selected: [...prevState.selected, user]
+      }
+    })
+  }
+
+  const removeSharedUser = (id: number) => {
+    const user = users.selected.find(user => user.id === id)
+    if (user === undefined) {
+      return
+    }
+
+    setUsers((prevState) => {
+      return {
+        available: [...prevState.available, user],
+        selected: prevState.selected.filter(user => user.id !== id)
+      }
+    })
+  }
+
+  const handleSubmit = (data: SnippetFormType) => {
+    if (users.selected.length > 1) {
+      data.sharedWith = users.selected.map(user => { return { id: user.id } })
+    }
+    console.log(data)
+    mutate(data)
+  }
+
+  return {
+    userIsLogged,
+    handleSubmit,
+    removeSharedUser,
+    addSharedUser,
+    decrementExpireDay,
+    incrementExpireDay,
+    isPending,
+    form,
+    users
+  }
+}
--- client/src/pages/home/SnippetForm.tsx
+++ client/src/pages/home/SnippetForm.tsx
@@ -5,134 +5,14 @@ import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Toaster } from "@/components/ui/toaster";
-import data from '@/data/users.json';
-import { SnippetFormType, createSnippetSchema } from "@/helpers/createSnippetSchema";
-import { useGlobalContext } from "@/hooks/useGlobalContext";
-import snippetAPI from "@/services/snippets";
-import { type Snippet as SnippetType } from "@/types/snippet";
-import { User } from "@/types/user";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useCreateSnippetForm } from "@/hooks/useCreateSnippetForm";
 import { ExpireDayButton } from "./ExpireDayButton";
 import { HoverFormSaver } from "./HoverFormSaver";
 import { PeopleSelectedList } from "./createSnippetForm/PeopleSelectedList";
 import { PeopleList } from "./createSnippetForm/peopleList";
 
 export function SnippetForm() {
-  const { userIsLogged, getToken } = useGlobalContext()
-  const form = useForm<SnippetFormType>({
-    resolver: zodResolver(createSnippetSchema),
-    defaultValues: {
-      content: "",
-      title: "",
-      expires: 1,
-      sharedWith: []
-    }
-  })
-  const queryClient = useQueryClient()
-  const [users, setUsers] = useState<{ available: User[], selected: User[] }>({ available: data, selected: [] })
-
-  const { isPending, mutate } = useMutation({
-    mutationKey: ["snippets"],
-    mutationFn: (data: SnippetFormType) => snippetAPI.createSnippet(data, getToken()),
-    onMutate: async (newSnippet: SnippetFormType) => {
-      await queryClient.cancelQueries({ queryKey: ["snippets"] })
-      const previousSnippets = queryClient.getQueryData(["snippets"])
-
-      queryClient.setQueryData(["snippets"], (oldData: SnippetType[]) => {
-        if (oldData === null) {
-          return [newSnippet]
-        }
-        return [
-          {
-            ...newSnippet,
-            id: oldData.length + 1,
-            isRecentlyAdded: true
-          },
-          ...oldData,
-        ]
-      })
-      return { previousSnippets }
-    },
-    onError: (error, _, context) => {
-      console.log(error)
-      if (context?.previousSnippets !== undefined) {
-        queryClient.setQueryData(["snippets"], context.previousSnippets)
-      }
-    },
-    onSettled: async () => {
-      await queryClient.invalidateQueries({
-        queryKey: ["snippets"]
-      })
-    },
-    onSuccess: () => {
-      console.log("Snippet added succesfully")
-      form.reset()
-      setUsers((prevState) => {
-        return {
-          available: prevState.available.concat(prevState.selected),
-          selected: []
-        }
-      })
-    },
-  });
-
-  const incrementExpireDay = () => {
-    const currentValue = form.getValues("expires")
-    form.setValue("expires", Number(currentValue) + 1)
-  }
-
-  const decrementExpireDay = () => {
-    const currentValue = form.getValues("expires")
-    if (currentValue === undefined) {
-      form.setValue("expires", 1)
-      return
-    }
-    if (currentValue <= 1) {
-      return
-    }
-    form.setValue("expires", Number(currentValue) - 1)
-  }
-
-  const addSharedUser = (id: number) => {
-    const user = users.available.find(u => u.id === id)
-    if (user === undefined) {
-      return
-    }
-
-    setUsers((prevState) => {
-      return {
-        available: prevState.available.filter(user => user.id !== id),
-        selected: [...prevState.selected, user]
-      }
-    })
-  }
-
-  const removeSharedUser = (id: number) => {
-    const user = users.selected.find(user => user.id === id)
-    if (user === undefined) {
-      return
-    }
-
-    setUsers((prevState) => {
-      return {
-        available: [...prevState.available, user],
-        selected: prevState.selected.filter(user => user.id !== id)
-      }
-    })
-  }
-
-  const handleSubmit = (data: SnippetFormType) => {
-    if (users.selected.length > 1) {
-      data.sharedWith = users.selected.map(user => { return { id: user.id } })
-    }
-    console.log(data)
-    mutate(data)
-  }
-
-
+  const { form, users, addSharedUser, decrementExpireDay, handleSubmit, incrementExpireDay, isPending, removeSharedUser, userIsLogged } = useCreateSnippetForm()
 
   return (
     <section className="fixed w-1/4 flex flex-col gap-4 items-center justify-center">
